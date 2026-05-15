fcad3743e412410a19b27bbe873e50817130192c
MAC <mac@MacBook-Air-MAC.local>
2026-05-15 16:26:10 +0300

    fix: видалено тимчасові скрипти, які ламали білд Next.js

--- test_coins.ts
+++
@@ -1,8 +0,0 @@
-import { getAdminDb } from './src/lib/firebaseAdmin';
-
-async function run() {
-  const db = getAdminDb();
-  const snap = await db.collection('coins').get();
-  console.log("Total coins in DB:", snap.size);
-}
-run();
--- test_db.ts
+++
@@ -1,8 +0,0 @@
-import { getAdminDb } from './src/lib/firebaseAdmin';
-
-async function run() {
-  const db = getAdminDb();
-  const snap = await db.collection('coin_catalog_meta').get();
-  snap.forEach(doc => console.log(doc.id, doc.data().count));
-}
-run();
